0fd44aaef875694b785ba1c819f29e486365dee9
Taemin Kim <102213033+Taemin98@users.noreply.github.com>
2022-04-04 22:29:43 +0900

    [Bronze II] Title: 하얀 칸, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1100. 하얀 칸/하얀 칸.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+    char chess[8][8];
+    int count = 0;
+
+    for(int i = 0; i < 8; i++){
+        for(int j = 0; j < 8; j++) scanf(" %c", &chess[i][j]);   
+    }
+
+    for(int i = 0; i < 8; i++){
+        for(int j = 0; j < 8; j++){
+            if((i + j) % 2 == 0 && chess[i][j] == 'F') count++;
+        }
+    } 
+
+    printf("%d", count);
+
+    return 0;
+}
