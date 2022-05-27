8c7b57b7523b1d2b783e7822765444643f8fa9dd
Taemin Kim <102213033+Taemin98@users.noreply.github.com>
2022-05-27 16:36:09 +0900

    [Bronze III] Title: 도미노, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2921. 도미노/도미노.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+    int N;
+    scanf("%d", &N);
+    int sum = 0;
+
+    for(int i = 1; i <= N; i++){
+        for(int j = 0; j <= i; j++){
+            sum = sum + i + j;
+        }
+    }
+    printf("%d", sum);
+
+    return 0;
+}
