d708ad8b995b04829dd85d1fb616d75df39b8094
Taemin Kim <102213033+Taemin98@users.noreply.github.com>
2022-06-26 23:26:47 +0900

    [Bronze III] Title: 할로윈의 사탕, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10178. 할로윈의 사탕/할로윈의 사탕.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    int T;
+    scanf("%d", &T);
+    int c, v;
+    for(int i = 0; i < T; i++){
+        scanf("%d %d", &c, &v);
+        printf("You get %d piece(s) and your dad gets %d piece(s).\n", c/v, c - (c/v)*v);
+    }
+    return 0;
+}
