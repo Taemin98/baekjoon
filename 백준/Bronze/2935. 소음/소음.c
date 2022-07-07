3933910b24abf0e3f2c2e381a8a1fbcb4deb81ae
Taemin Kim <102213033+Taemin98@users.noreply.github.com>
2022-07-07 11:42:11 +0900

    [Bronze III] Title: 소음, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Bronze/2935. 소음/소음.c
+++ 백준/Bronze/2935. 소음/소음.c
@@ -1,30 +1,25 @@
-// 소음
-
 #include<stdio.h>
 #include<string.h>
+#include<stdlib.h>
 
-// void arrycpy(char a[], char b[]){
-//     for(int i = 0; b[i] != '\0'; i++){
-//         a[i] = b[i];
-//     }
-// }
+void arrycpy(char a[], char b[]){
+    for(int i = 0; b[i] != '\0'; i++){
+        a[i] = b[i];
+    }
+}
 
 int main(){
-    char A[101] = {0,};
-    char B[101] = {0,};
-    char C[201] = {0,};
+    char A[101] = {0};
+    char B[101] = {0};
+    char C[201] = {0};
     char op;
 
     scanf(" %s", A);
     scanf(" %c", &op);
     scanf(" %s", B);
 
-    if(strlen(A) >= strlen(B)){
-        for(int i = 0; A[i] != '\0'; i++) C[i] = A[i];
-    }
-    else{
-        for(int i = 0; B[i] != '\0'; i++) C[i] = B[i];
-    }
+    if(strlen(A) >= strlen(B)) arrycpy(C, A);
+    else arrycpy(C, B);
 
     if(op == '*'){
         printf("%d", 1);
@@ -33,11 +28,10 @@ int main(){
 
     if(op == '+'){
         if( strlen(A) == strlen(B) ) C[0] = '2';
-        else if( strlen(A) > strlen(B) ) C[(strlen(A) - strlen(B))] = '1';
-        else C[(strlen(B) - strlen(A))] = '1';
-        // for(int i = 0; C[i] != '\0'; i++) printf("%d", C[i] - '0');
-        for(int i = 0; C[i] != '\0'; i++) printf("%c", C[i]);
+        else if( strlen(A) > strlen(B) ) C[(strlen(A)-strlen(B))] = '1';
+        else C[(strlen(B)-strlen(A))] = '1';
+        for(int i = 0; C[i] != '\0'; i++) printf("%d", C[i] - '0');
     }
+    
     return 0;
 }
-// 1000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
